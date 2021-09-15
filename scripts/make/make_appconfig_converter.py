b18c948d7cfb89166d5f0aa4e5c288bb6e9e252a
xz-dev <xiangzhedev@gmail.com>
2021-09-15 08:27:22 +0800

    ci: 移除配置生成脚本对 Github API 的调用

--- scripts/make/make_appconfig_converter.py
+++ scripts/make/make_appconfig_converter.py
@@ -1,15 +1,11 @@
+from sys import argv
 from make_appconfig import mk_config
-import requests
-
-api_url = "https://api.github.com/repos/JohnsonRan/UpgradeAll-rules/issues/1"
 
 
-def convert_issues(api_url):
-    input_text = requests.get(api_url).json()['body']
-    print(input_text)
-    for json_name, json_text in mk_config(input_text).items():
+def convert_issues_body(body_text):
+    for json_name, json_text in mk_config(body_text).items():
         with open(f'./rules/apps/{json_name}.json', 'w+') as f:
             f.write(json_text)
 
 
-convert_issues(api_url)
+convert_issues_body(argv[0])
