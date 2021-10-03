90b9a3424cbb6e54945fd39eddb2ad5cff7f621a
xz-dev <xiangzhedev@gmail.com>
2021-10-03 10:48:59 +0800

    fix: 指定以 UTF-8 输出文件

--- scripts/make/make_appconfig_converter.py
+++ scripts/make/make_appconfig_converter.py
@@ -4,7 +4,8 @@ from make_appconfig import mk_config
 
 def convert_issues_body(body_text):
     for json_name, json_text in mk_config(body_text).items():
-        with open(f'./rules/apps/{json_name}.json', 'w+') as f:
+        with open(f'./rules/apps/{json_name}.json', 'w+',
+                  encoding="utf-8") as f:
             f.write(json_text)
 
 
